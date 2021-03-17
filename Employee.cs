4a68b793da9707d74c0926dc37945cdd8478ce52
oladunnijakes1 <78372122+oladunnijakes1@users.noreply.github.com>
2021-03-17 14:49:29 +0000

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+
+        private decimal salary;
+        private string firstName;
+        private string lastName;
+
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+        public Employee(string firstName, string lastName, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 0)
+                    salary = value;
+            }
+        }
+    }
+}
